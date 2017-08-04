344b943efbfcd8a6553929389cdc73da0b658cab
ompi <ompologus@gmail.com>
2017-08-04 18:22:04 +0200

    added 5x7 nixie font

---
+++ nixie_font.h
@@ -0,0 +1,118 @@
+#include <stdint.h>
+
+#define _____ 0x00,
+#define ____X 0x01,
+#define ___X_ 0x02,
+#define ___XX 0x03,
+#define __X__ 0x04,
+#define __X_X 0x05,
+#define __XX_ 0x06,
+#define __XXX 0x07,
+#define _X___ 0x08,
+#define _X__X 0x09,
+#define _X_X_ 0x0a,
+#define _X_XX 0x0b,
+#define _XX__ 0x0c,
+#define _XX_X 0x0d,
+#define _XXX_ 0x0e,
+#define _XXXX 0x0f,
+#define X____ 0x10,
+#define X___X 0x11,
+#define X__X_ 0x12,
+#define X__XX 0x13,
+#define X_X__ 0x14,
+#define X_X_X 0x15,
+#define X_XX_ 0x16,
+#define X_XXX 0x17,
+#define XX___ 0x18,
+#define XX__X 0x19,
+#define XX_X_ 0x1a,
+#define XX_XX 0x1b,
+#define XXX__ 0x1c,
+#define XXX_X 0x1d,
+#define XXXX_ 0x1e,
+#define XXXXX 0x1f,
+
+uint8_t nixieFontData[] = {
+
+    _XXX_
+    X___X
+    X___X
+    X___X
+    X___X
+    X___X
+    _XXX_
+
+    __X__
+    _XX__
+    X_X__
+    __X__
+    __X__
+    __X__
+    __X__
+
+    _XXX_
+    X___X
+    ____X
+    _XXX_
+    X____
+    X____
+    XXXXX
+
+    XXXXX
+    ___X_
+    __X__
+    ___X_
+    ____X
+    X___X
+    _XXX_
+
+    ___X_
+    __XX_
+    _X_X_
+    X__X_
+    XXXXX
+    ___X_
+    ___X_
+
+    XXXXX
+    X____
+    X____
+    _XXX_
+    ____X
+    X___X
+    _XXX_
+
+    ____X
+    ___X_
+    __X__
+    _XXX_
+    X___X
+    X___X
+    _XXX_
+
+    XXXXX
+    ____X
+    ____X
+    ___X_
+    __X__
+    _X___
+    X____
+
+    _XXX_
+    X___X
+    X___X
+    _XXX_
+    X___X
+    X___X
+    _XXX_
+
+    _XXX_
+    X___X
+    X___X
+    _XXX_
+    __X__
+    _X___
+    X____
+
+};
